c8fce6862c39840b70a65d6f9d8840ebb6cc56df
wwj <wwj_computeryes@163.com>
2017-11-16 22:44:13 +0800

    提交项目到GIT

---
+++ app/src/main/java/com/wwj/service/lifecycle/MainActivity.java
@@ -0,0 +1,74 @@
+package com.wwj.service.lifecycle;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button startService,stopService,bindService,unbindService;
+    private final String TAG=getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        startService=(Button)findViewById(R.id.startService);
+        stopService=(Button)findViewById(R.id.stopService);
+        bindService=(Button)findViewById(R.id.bindService);
+        unbindService=(Button)findViewById(R.id.unbindService);
+
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(this, ServiceLifecycle.class);
+        switch (v.getId()){
+            case R.id.startService:
+                intent.putExtra("hello","hello world");
+                startService(intent);
+                break;
+            case R.id.stopService:
+                stopService(intent);
+                break;
+            case R.id.bindService:
+                bindService(intent,mServcieConnection,BIND_AUTO_CREATE);
+                break;
+            case R.id.unbindService:
+                unbindService(mServcieConnection);
+                break;
+        }
+    }
+
+    private ServiceConnection mServcieConnection=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "===========onServiceConnected===========");
+            ServiceLifecycle.MyBinder serviceLifecycle=(ServiceLifecycle.MyBinder)service;
+            int result=serviceLifecycle.getService().add(5,6);
+            int state=serviceLifecycle.getService().getServiceState();
+//            Log.d(TAG, "===========result="+ result+"========state="+state);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "===========onServiceDisconnected===========");
+        }
+    };
+
+}
---
+++ app/src/main/java/com/wwj/service/lifecycle/ServiceLifecycle.java
@@ -0,0 +1,72 @@
+package com.wwj.service.lifecycle;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/11/16 0016.
+ */
+public class ServiceLifecycle extends Service {
+
+    private final String TAG=getClass().getSimpleName();
+    private int STATE=0;
+    private final int SERVICE_CREATE=1;
+    private final int SERVICE_DESTROY=2;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG,"===========onBind============");
+        return new MyBinder();
+    }
+
+    class  MyBinder extends Binder{
+        ServiceLifecycle getService(){
+            return ServiceLifecycle.this;
+        }
+    }
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG,"===========onUnbind============");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String values=intent.getStringExtra("hello");
+        Log.d(TAG,"===========onStartCommand===========values="+values);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+//    @Override
+//    public void onStart(Intent intent, int startId) {
+//        super.onStart(intent, startId);
+//    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        STATE=SERVICE_CREATE;
+        Log.d(TAG, "===========onCreate========STATE="+STATE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        STATE=SERVICE_DESTROY;
+        Log.d(TAG, "===========onDestroy==========STATE="+STATE);
+    }
+
+    public int getServiceState(){
+        return STATE;
+    }
+}
